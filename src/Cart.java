63e4fb17ea93ebb59d63d8a8b9a1e48f1d5beaae
Navindran <147373039+NavindranRaghupathy@users.noreply.github.com>
2024-06-11 09:19:49 +0800

    Update Cart 1

--- src/Cart.java
+++ src/Cart.java
@@ -5,7 +5,7 @@ class Cart{
     private ArrayList<Product> product;
     private ArrayList<Integer> quantity;
     private ArrayList<Float> price;
-    private double totalPrice , afterDisc;
+    private double totalPrice=0 , afterDisc;
     private String date_created;
 
     public Cart(String cart_id ,float totalPrice , String date_created)
@@ -14,6 +14,8 @@ class Cart{
         product = new ArrayList<>();
         this.totalPrice = totalPrice;
         this.date_created = date_created;
+        quantity = new ArrayList<>();
+        price = new ArrayList<>();
     }
 
     public void addItem(Product p , int quantity , float price)
@@ -21,7 +23,7 @@ class Cart{
         product.add(p);
         this.quantity.add(quantity);
         this.price.add(price);
-        totalPrice += price;
+        totalPrice += price*quantity;
     }
 
     public void removeItem(Product p)
@@ -33,7 +35,7 @@ class Cart{
                 product.remove(i);
                 quantity.remove(i);
                 price.remove(i);
-                totalPrice -= price.get(i);
+                totalPrice -= price.get(i)*quantity.get(i);
                 break;
             } 
         }
@@ -49,9 +51,21 @@ class Cart{
             afterDisc = totalPrice - (totalPrice * 0.3);
     }
 
+
+
     public void printCart()
     {
-
+        System.out.println("---------------------------------------------------------------------------------------------------");
+        System.out.println("Cart ID : " + cart_id);
+        System.out.println("Date created : " + date_created);
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s \n" , "Items" , "Brand" , "Quantity" , "Price" , "Amount");
+        for(int i=0;i<product.size();i++)
+        {
+            // System.out.printf("%-10s %-10s %-10d %-10.2f %-10.2f \n" , product.get(i).getPName() , product.get(i).getBrand() , quantity.get(i) , price.get(i) , ((quantity.get(i))*(price.get(i))));
+        }
+        discount();
+        System.out.printf("%30s RM%2.2f \n" , "Total Price" , afterDisc);
+        System.out.println("---------------------------------------------------------------------------------------------------");
     }
 
     public void checkout()
