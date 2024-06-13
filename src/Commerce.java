9ae59b534fcca86673d9205b5357c8ef461b5c7e
kudan <kumuhddaniel17@gmail.com>
2024-06-13 22:46:33 +0800

    main

--- src/Commerce.java
+++ src/Commerce.java
@@ -1,7 +1,16 @@
+import java.util.*;
+
 public class Commerce {
     public static void main(String[] args)
     {
-        Admin admin = new Admin();
-        
+        int category;
+        System.out.println("Enter your user category : ");
+        System.out.println("1. Customer");
+        System.out.println("2. Seller");
+        System.out.println("3. Admin");
+        System.out.println("4. Shipping");
+        System.out.println("Choose category : ");
+        Scanner scanner = new Scanner(System.in);
+        category = scanner.nextInt();
     }
 }
