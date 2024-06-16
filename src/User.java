d75146efe71ec1d3e91d713449e2f8fb07faa79f
Navindran <147373039+NavindranRaghupathy@users.noreply.github.com>
2024-06-16 15:50:52 +0800

    Update User 2

--- src/User.java
+++ src/User.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class User {
     private String id;
     private String name;
@@ -13,11 +16,34 @@ class User {
         this.pass = pass;
     }
 
+    public String getName(){return name;}
+    public String getID(){return id;}
+    public String getEmail(){return email;}
+    public String getUsername(){return username;}
+    public String getPass(){return pass;}
+    public String getAddress() {return null;}
+    public String getPhoneNo() {return null;}
+    public String getStoreName() {return null;}
+
     public void viewProduct() {}
 
-    public void Login() {}
+    public void ReadServiceProduct(Scanner in){}
+    public void ReadPhysicalProduct(Scanner in){}
+
+    public void ReadCustPhysical(ArrayList<Physical_Goods> p){}
+    public void ReadCustService(ArrayList<Services> s){}
+
+    public void SignUp(ArrayList<User> user , Scanner sc) {}
 
     public void Logout() {}
 
     public void updateProfile() {}
+
+    public void addProduct(Scanner sc){}
+    public void updateProduct(Scanner sc){}
+    public void deleteProduct(Scanner sc){}
+
+    public void viewOrder(){}
+    public void buyProducts(Scanner sc){}
+    public void checkout(){}
 }
