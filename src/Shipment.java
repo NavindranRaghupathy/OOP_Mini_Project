107d2d77abd3490ba01d471e3d36b4fe182b3fc1
Navindran <147373039+NavindranRaghupathy@users.noreply.github.com>
2024-06-20 15:48:59 +0800

    Update Shipment 10

--- src/Shipment.java
+++ src/Shipment.java
@@ -1,18 +1,23 @@
 class Shipment {
     private String shipping_id;
     private String ship_address;
-    private String ship_method;
-    private String shipping_cost;
-    private String date_ship;
-    private String date_deliver;
+    private String custId;
+    private String ProductId;
+    private String ProductName;
+    private int quantity;
+    
+    
 
-    public Shipment(String shipping_id, String ship_address, String ship_method, String shipping_cost, String date_ship, String date_deliver) {
-        this.shipping_id = shipping_id;
+
+    public Shipment(String shipping_id, String ship_address,String custId,String ProductId,String ProductName ,int quantity) {
+        this.shipping_id = shipping_id;  
         this.ship_address = ship_address;
-        this.ship_method = ship_method;
-        this.shipping_cost = shipping_cost;
-        this.date_ship = date_ship;
-        this.date_deliver = date_deliver;
+        this.custId = custId; 
+        this.ProductId = ProductId; 
+        this.ProductName = ProductName; 
+        this.quantity = quantity;
+      
+      
     }
 
     // Setters
@@ -24,22 +29,8 @@ class Shipment {
         this.ship_address = ship_address;
     }
 
-    public void setMethod(String ship_method) {
-        this.ship_method = ship_method;
-    }
-
-    public void setShippingCost(String shipping_cost) {
-        this.shipping_cost = shipping_cost;
-    }
-
-    public void setDateShip(String date_ship) {
-        this.date_ship = date_ship;
-    }
-
-    public void setDateDeliver(String date_deliver) {
-        this.date_deliver = date_deliver;
-    }
-
+   
+   
     // Getters
     public String getShippingId() {
         return shipping_id;
@@ -48,30 +39,21 @@ class Shipment {
     public String getAddress() {
         return ship_address;
     }
+    
+    public String getCustId(){return custId;}
+    public String getProductId(){return ProductId;}
+    public String getProducName(){return ProductName;}
+    public int getQuantity(){return quantity;}
 
-    public String getMethod() {
-        return ship_method;
-    }
+   
 
-    public String getShippingCost() {
-        return shipping_cost;
-    }
-
-    public String getDateShip() {
-        return date_ship;
-    }
-
-    public String getDateDeliver() {
-        return date_deliver;
-    }
+   
 
     // Methods
     public void printShipping() {
         System.out.println("Shipping ID: " + shipping_id);
         System.out.println("Address: " + ship_address);
-        System.out.println("Method: " + ship_method);
-        System.out.println("Shipping Cost: " + shipping_cost);
-        System.out.println("Date Shipped: " + date_ship);
-        System.out.println("Date Delivered: " + date_deliver);
+       
+        
     }
 }
