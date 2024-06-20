19dd74a0365bc2ba51b42784161dd680e2a71f7a
Navindran <147373039+NavindranRaghupathy@users.noreply.github.com>
2024-06-20 15:46:50 +0800

    Update Physical_Goods 100

--- src/Physical_Goods.java
+++ src/Physical_Goods.java
@@ -6,7 +6,6 @@ class Physical_Goods extends Product{
     private int quantity_available;
     private static ArrayList<Review> review;
 
-    private static ArrayList<Physical_Goods> products = new ArrayList<>(); //to add,remove product
 
     public Physical_Goods(String s_id , String p_id,String p_name,float price,
                          int quantity_available,String date_added,String brand,
@@ -55,40 +54,6 @@ class Physical_Goods extends Product{
         }
         
      }
-
-    // public void addProduct(Physical_Goods product) {
-    //     products.add(product);
-    //     System.out.println("Product added successfully.");
-    // }
-
-    // public void updateProduct(String p_id, String p_name, float price,
-    //                           int quantity_available,  String date_added, String brand,
-    //                           float weight) {   //cannot send an object,with this we analyse one by one to update any changes.
-    //     for (Physical_Goods product : products) {
-    //         if (product.getPId().equals(p_id)) { //Means product ID should all be the correct one,so i refer product ID.If wanna change product id , i think need to do another function for that.
-    //             product.setPName(p_name);
-    //             product.setPrice(price);
-    //             this.quantity_available = quantity_available;
-    //             product.setDate(date_added);
-    //             product.setBrand(brand);
-    //             product.setWeight(weight);
-    //             System.out.println("Product updated successfully.");
-    //             return;
-    //         }
-    //     }
-    //     System.out.println("Product not found.");
-    // }
-
-    // public void removeProduct(String p_id) { //usig arraylist , easier to remove product using productid
-    //     for (Physical_Goods product : products) {
-    //         if (product.getPId().equals(p_id)) {
-    //             products.remove(product);
-    //             System.out.println("Product removed successfully.");
-    //             return;
-    //         }
-    //     }
-    //     System.out.println("Product not found.");
-    // }
 
     public void printDetails() {
 
